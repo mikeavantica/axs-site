60333ab9e74dd5ff00a3820eb49bd8dbe2160ce5
Maikol Guzman <mikeguzman@gmail.com>
2014-06-12 23:06:27 -0600

    First HTML Mockup

---
+++ sites/all/themes/auxis_main/templates/region--footer.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+<?php if ($content): ?>
+  <footer id="footer" class="<?php print $classes; ?>">
+      <div class="footer-wrapper">
+          <div class="block-footer-menu">footer menu</div>
+          <div class="block-footer-search">search</div>
+      </div>
+  </footer>
+<?php endif; ?>
